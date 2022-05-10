4114b9d146d947e9170c3c4378f82e6314a07807
VinodN13 <100939872+VinodN13@users.noreply.github.com>
2022-05-10 18:43:38 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World! Good evening")
+    fmt.Fprintf(w, "Hello World! Good night")
 }
 
 func main() {
